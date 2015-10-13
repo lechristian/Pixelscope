e33235ba9575cf2d06d5e6b36d56f1600729fb80
Christian Le <christianle94@gmail.com>
2015-10-12 19:46:19 -0700

    Initial Commit

---
+++ Pixelscope/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pixelscope
+//
+//  Created by Christian Le on 10/12/15.
+//  Copyright © 2015 innovativedesign. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
